18efab3deaa814a8242f6079d873442b93650b70
debbie-leung <dsl2162@columbia.edu>
2021-03-28 18:45:20 -0400

    Updated healthcheck file

--- healthcheck.py
+++ healthcheck.py
@@ -10,11 +10,15 @@ LX16A.initialize('/dev/ttyUSB0')
 
 servos = f.readMotor()
 
-# Query motor position
-def 
+for i in range(8):
+    # Query motor position
+    if not (120 <= servos[i].getPhysicalPos() <= 240):
+        print("Motor %d is %f and not in range" % (servos[i].IDRead(), servos[i].getPhysicalPos()))
+        servos[i].moveTimeWrite(120, time=5000)
 
-# Query motor temp
-
-# Query motor current
-
-# Query 
+    # Query motor temp
+    # Query motor current
+    print("motor: %d voltage: %f temperature: %f" % (servos[i].IDRead(), servos[i].vInRead(), servos[i].tempRead()))
+           
+# Check communication error by printing to terminal
+print("Established communication")
